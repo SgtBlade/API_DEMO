36318dd8f624f5f780ebea09291c405f697da216
SgtBlade <migueleken@hotmail.com>
2023-05-23 21:48:16 +0200

    first commit

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
 import "dotenv/config";
 import express from "express";
-import { ObjectId } from "mongodb";
-import { initClient } from "./db/mongo.js";
-import { registerMiddleware } from "./middleware/index.js";
-import passport from "passport";
-import  LocalStrategy from "./middleware/auth/LocalStrategy.js";
-import JwtStrategy from "./middleware/auth/JwtStrategy.js";
-import jwt from "jsonwebtoken";
-import { registerRoutes } from "./routers/routers.js";
 
 //Create an Express app:
 const app = express();
 const port = 3002;
 
-//Register middleware:
-registerMiddleware(app);
-
-//Initialize MongoDB client and database:
-app.use(passport.initialize());
-// Use LocalStrategy to verify the user credentials locally
-passport.use("local", LocalStrategy);
-
-// Use JwtStrategy to verify the user credentials with a JWT token
-passport.use("jwt", JwtStrategy);
-
-registerRoutes(app)
 
 //Start the server and handle server crashes:
 const server = app.listen(port, () => {
